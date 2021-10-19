eace46b0756452896f6c14ef77d232e6a7b0810c
insanz01 <insankamil002@gmail.com>
2021-10-19 18:51:11 +0700

    Update icon

--- application/views/template.php
+++ application/views/template.php
@@ -152,11 +152,11 @@
 				</p>
 				<div class="credits">
 					<!--
-            You are NOT allowed to delete the credit link to TemplateMag with free version.
-            You can delete the credit link only if you bought the pro version.
-            Buy the pro version with working PHP/AJAX contact form: https://templatemag.com/dashio-bootstrap-admin-template/
-            Licensing information: https://templatemag.com/license/
-          -->
+			            You are NOT allowed to delete the credit link to TemplateMag with free version.
+			            You can delete the credit link only if you bought the pro version.
+			            Buy the pro version with working PHP/AJAX contact form: https://templatemag.com/dashio-bootstrap-admin-template/
+			            Licensing information: https://templatemag.com/license/
+			          -->
 					Created with Dashio template by <a href="https://templatemag.com/">TemplateMag</a>
 				</div>
 				<a href="blank.html#" class="go-top">
